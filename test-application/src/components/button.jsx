f51f56ab64a23d0504e0fa8282a4b4218eec8854
Elvis <therealelvisrivera@gmail.com>
2024-08-25 13:51:05 -0500

    button creations

---
+++ test-application/src/components/button.jsx
@@ -0,0 +1,5 @@
+function Button() {
+    return <button className='btn'>Trying to place button</button>;
+}
+
+export default Button;
---
+++ test-application/src/components/card.jsx
@@ -0,0 +1,14 @@
+import Button from "./button";
+
+function Card() {
+    return (
+        <div className="card">
+            <h1>Card Name</h1>
+            <p>Card Description</p>
+            <Button />
+
+        </div>
+    )
+}
+
+export default Card;
